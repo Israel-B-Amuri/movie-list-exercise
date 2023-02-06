2f2e3fe33cba5cf44842c3b2c8efb5547e786903
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-02-06 13:48:28 +0200

    adding the fetch function

--- public/script.js
+++ public/script.js
@@ -1,8 +1,34 @@
+const container = document.querySelector("#container");
+
 async function fetchData() {
   const response = await fetch("/api/movies");
   const data = await response.json();
-  const array = data.map((movie) => movie.title);
-  console.log(array);
+
+  container.innerHTML = `
+  <table style="width:90%">
+    <tr>
+        <th>Title</th>
+        <th>Year</th>
+        <th>Age Rating</th>
+        <th>Genre</th>
+        <th>Rating</th>
+    </tr>
+   
+    ${data
+      .map(
+        (movie) =>
+          `<tr>
+        <td>${movie.title}</td>
+        <td>${movie.releaseDate}</td>
+        <td>${movie.age}</td>
+        <td>${movie.genre}</td>
+        <td>${movie.rating * 100}%</td></tr>`
+      )
+      .join("")};
+
+  </table>
+  
+  `;
 }
 
 fetchData();
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const path = require('path');
-const movies = require('./data/movies.json');
+const express = require("express");
+const path = require("path");
+const movies = require("./data/movies.json");
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join('public')));
+app.use(express.static(path.join("public")));
 
-app.get('/api/movies', (request, response) => {
-  response.status(200).json(movies.map(movie => ({
-    id: movie.id,
-    title: movie.title,
-    age: movie.age,
-    genre: movies.genre,
-    releaseDate: movie.releaseDate,
-    rating: movie.rating
-  })));
+app.get("/api/movies", (request, response) => {
+  response.status(200).json(
+    movies.map((movie) => ({
+      id: movie.id,
+      title: movie.title,
+      age: movie.age,
+      genre: movies.genre,
+      releaseDate: movie.releaseDate,
+      rating: movie.rating,
+    }))
+  );
 });
 
 app.listen(port, (error) => {
-  if(error) {
+  if (error) {
     console.log(error);
     return;
   }
